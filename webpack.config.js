811eb111b303ed8f1a3d895891f3a5657fccb6b9
Stephen Blanco <sjblanc0000@gmail.com>
2025-01-01 18:18:25 -0500

    setup typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,25 @@
 const path = require("path")
 
 module.exports = { 
-    "entry": "./src/main.js",
-    "output": {
-        "filename": "main.js",
-        "path": path.resolve(__dirname, "dist")
+    context: __dirname,
+    entry: "./src/main.ts",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        publicPath: "/dist/"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "ts-loader"
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".ts"]
     }
 }
